caee4e06eed56b94f1d36c794dd73398e0ae8076
David <dacero1@cuc.edu.co>
2017-09-16 16:02:44 -0500

    App Terminada

--- app/src/main/java/com/example/android/appventazapatos/MainActivity.java
+++ app/src/main/java/com/example/android/appventazapatos/MainActivity.java
@@ -16,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
     private String genZap[];
     private Spinner tipoZapatos;
     private String tipoZap[];
+    private Spinner marcZap;
+    private String marcaZap[];
     private Resources resources;
 
 
@@ -28,19 +30,107 @@ public class MainActivity extends AppCompatActivity {
         cantidad = (EditText) findViewById(R.id.txtCantidad);
         genero = (Spinner) findViewById(R.id.SpnGenero);
         tipoZapatos=(Spinner)findViewById(R.id.SpnTIpoCalzado);
+        marcZap=(Spinner)findViewById(R.id.SpnMarCalzado);
         resources = this.getResources();
         genZap=resources.getStringArray(R.array.Genero);
         tipoZap=resources.getStringArray(R.array.tipoZapato);
+        marcaZap=resources.getStringArray(R.array.marcaZapatos);
         ArrayAdapter<String>adapterG=new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,genZap);
         ArrayAdapter<String>adapterT=new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,tipoZap);
+        ArrayAdapter<String>adapterM=new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,marcaZap);
         genero.setAdapter(adapterG);
         tipoZapatos.setAdapter(adapterT);
+        marcZap.setAdapter(adapterM);
     }
 
     public void calcular(View v){
-        int opcion, resultado=0;
-        res.setText("");
-        opcion = genero.getSelectedItemPosition();
+        if(validad()){
+            int opcionG,opcionT,opcionM, resultado=0,cant;
+            res.setText("");
+            opcionG = genero.getSelectedItemPosition();
+            opcionT=tipoZapatos.getSelectedItemPosition();
+            opcionM=marcZap.getSelectedItemPosition();
+            cant=Integer.parseInt(cantidad.getText().toString());
+            switch (opcionG){
+                case 0://en caso de que sea para hombre
+                    switch (opcionT){
+                        case 0:
+                            switch (opcionM){
+                                case 0:
+                                      resultado=cant*120000;
+                                    break;
+                                case 1:
+                                    resultado=cant*140000;
+                                    break;
+                                case 2:
+                                    resultado=cant*130000;
+                                    break;
+                            }
+                            break;
+                        case 1:
+                            switch (opcionM){
+                                case 0:
+                                    resultado=cant*50000;
+                                    break;
+                                case 1:
+                                    resultado=cant*80000;
+                                    break;
+                                case 2:
+                                    resultado=cant*100000;
+                                    break;
+                            }
+                            break;
+                    }
+                    break;
+                case 1://en caso de que sea para mujer
+                    switch (opcionT){
+                        case 0:
+                            switch (opcionM){
+                                case 0:
+                                    resultado=cant*100000;
+                                    break;
+                                case 1:
+                                    resultado=cant*130000;
+                                    break;
+                                case 2:
+                                    resultado=cant*110000;
+                                    break;
+                            }
+                            break;
+                        case 1:
+                            switch (opcionM){
+                                case 0:
+                                    resultado=cant*80000;
+                                    break;
+                                case 1:
+                                    resultado=cant*70000;
+                                    break;
+                                case 2:
+                                    resultado=cant*120000;
+                                    break;
+                            }
+                            break;
+                    }
+                    break;
+            }
+            res.setText(""+resultado);
+        }
 
+    }
+    public void borrar(View v){
+        cantidad.setText("");
+        res.setText("");
+        genero.setSelection(0);
+        tipoZapatos.setSelection(0);
+        marcZap.setSelection(0);
+        cantidad.requestFocus();
+    }
+    private boolean validad(){
+        if(cantidad.getText().toString().isEmpty()){
+            cantidad.setError(resources.getString(R.string.error));
+            cantidad.requestFocus();
+            return false;
+        }
+        return true;
     }
 }
