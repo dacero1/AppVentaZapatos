83b717f0bda9ed67c1fa6ce264b5b208f0bc4eff
David <dacero1@cuc.edu.co>
2017-09-16 14:48:02 -0500

    Inicio de codificacion

--- app/src/main/java/com/example/android/appventazapatos/MainActivity.java
+++ app/src/main/java/com/example/android/appventazapatos/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.android.appventazapatos;
 
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Spinner tipoCalzadoFemenino;
+    private Spinner tipoCalzadoMasculino;
+    private Spinner marcaCalzadoFemenino;
+    private Spinner marcacalzadoMasculino;
+    private String tipoZ[];
+    private EditText cantidad;
+    private Resources resources;
+    private TextView res;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        res = (TextView)findViewById(R.id.txtResultado);
+        cantidad =(EditText) findViewById(R.id.txtCantidad);
+        tipoCalzadoFemenino = (Spinner)findViewById(R.id.SpnTipCalFemenino);
+        resources=this.getResources();
+        
     }
 }
